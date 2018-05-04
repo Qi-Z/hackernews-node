8ec43731cb8c254240cca3f7933e4817d4fcb66e
Qi Zhang <zhang.qi.js@gmail.com>
2018-05-04 03:28:54 -0400

    Add schema and resolver for Link

--- src/index.js
+++ src/index.js
@@ -1,19 +1,43 @@
-const { GraphQLServer } = require('graphql-yoga');
+const {GraphQLServer} = require('graphql-yoga');
 
 // 1 Type def
 // NOTE: The schema definition is actually string, which means graphql-yoga will have to parse the string.
 // maybe into some AST (Abstract Syntax Tree)
-// Every field is backe by a resolver function.
+// Every field is backed by a resolver function.
+// Here we write type definition as string. In fact, it can be put into .graphql files.
+// ID is special scalar type in graphql
 const typeDefs = `
 type Query {
     info: String!
+    feed: [Link!]!
+}
+
+type Link {
+    id: ID!
+    description: String!
+    url: String!
 }
 `;
 
+let links = [
+    {
+        id: 'link-0',
+        url: 'www.howtographql.com',
+        description: 'Fullstack tutorial for graphql'
+    }
+];
 // 2 Resolver functions
+// Resolver is extremely important for query, mutation, subscription to be resolved.
 const resolvers = {
     Query: {
-        info: () => `This is the API of a Hackernews Clone`
+        info: () => `This is the API of a Hackernews Clone`,
+        feed: () => links
+    },
+    Link: { // Link resolver is not necessary here. Just to show how resolver is managed in such hierarchy
+        // root is the result of previous resolver execution level
+        id: (root) => root.id,
+        url: (root) => root.url,
+        description: (root) => root.description
     }
 };
 
@@ -25,5 +49,5 @@ const server = new GraphQLServer(
         resolvers
     }
 );
-server.start(()=> console.log(`Server is running on http://localhost:4000`)); // Go to http://localhost:4000 to interact with GraphQL, or use POSTman CURL
+server.start(() => console.log(`Server is running on http://localhost:4000`)); // Go to http://localhost:4000 to interact with GraphQL, or use POSTman CURL
 
